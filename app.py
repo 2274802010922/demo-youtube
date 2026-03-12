f17526da87e33abf64990b1d01fbd311d1cca7ae
2274802010922 <habang5233@gmail.com>
2026-03-12 16:22:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,47 +4,82 @@ import pandas as pd
 
 API_KEY = "AIzaSyAK89AZdFqWG3NJmR1TnUbM1QUdrQWD6AM"
 
-st.title("YouTube Comment Extractor")
+st.title("YouTube Video & Comment Extractor")
 
-video_url = st.text_input("Enter YouTube Video URL")
+# search video
+query = st.text_input("Search video")
 
-if video_url:
+if query:
 
-    video_id = video_url.split("v=")[-1]
-
-    url = "https://www.googleapis.com/youtube/v3/commentThreads"
+    search_url = "https://www.googleapis.com/youtube/v3/search"
 
     params = {
         "part": "snippet",
-        "videoId": video_id,
-        "maxResults": 100,
+        "q": query,
+        "type": "video",
+        "maxResults": 5,
         "key": API_KEY
     }
 
-    response = requests.get(url, params=params)
+    response = requests.get(search_url, params=params)
     data = response.json()
 
-    comments = []
+    videos = []
 
     for item in data["items"]:
-        snippet = item["snippet"]["topLevelComment"]["snippet"]
+        title = item["snippet"]["title"]
+        video_id = item["id"]["videoId"]
 
-        comments.append({
-            "author": snippet["authorDisplayName"],
-            "comment": snippet["textDisplay"],
-            "likes": snippet["likeCount"]
+        videos.append({
+            "title": title,
+            "video_id": video_id
         })
 
-    df = pd.DataFrame(comments)
+    video_titles = [v["title"] for v in videos]
 
-    st.write("Comments")
-    st.dataframe(df)
+    selected_title = st.selectbox("Choose video", video_titles)
 
-    csv = df.to_csv(index=False).encode("utf-8-sig")
+    selected_video = next(v for v in videos if v["title"] == selected_title)
 
-    st.download_button(
-        "Download CSV",
-        csv,
-        "youtube_comments.csv",
-        "text/csv"
-    )
+    video_id = selected_video["video_id"]
+
+    st.video(f"https://youtube.com/watch?v={video_id}")
+
+    if st.button("Load Comments"):
+
+        comment_url = "https://www.googleapis.com/youtube/v3/commentThreads"
+
+        params = {
+            "part": "snippet",
+            "videoId": video_id,
+            "maxResults": 100,
+            "key": API_KEY
+        }
+
+        response = requests.get(comment_url, params=params)
+        data = response.json()
+
+        comments = []
+
+        for item in data["items"]:
+            snippet = item["snippet"]["topLevelComment"]["snippet"]
+
+            comments.append({
+                "author": snippet["authorDisplayName"],
+                "comment": snippet["textDisplay"],
+                "likes": snippet["likeCount"]
+            })
+
+        df = pd.DataFrame(comments)
+
+        st.subheader("Comments")
+        st.dataframe(df)
+
+        csv = df.to_csv(index=False).encode("utf-8-sig")
+
+        st.download_button(
+            "Download CSV",
+            csv,
+            "youtube_comments.csv",
+            "text/csv"
+        )
